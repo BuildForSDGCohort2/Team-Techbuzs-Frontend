9f98170c70491d79da1e360a820193f8f63f152d
Oreofe Solarin <oreofesolarin@gmail.com>
2020-10-19 20:14:08 +0100

    Create community.dart

---
+++ Flutter/lib/ui/views/community.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class LoginView extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  body: Container(child: Image.network(
+  'https://media.mktg.workday.com/is/image/workday/illustration-group-people-2?fmt=png-alpha&wid=1000',
+)));
+  
+  }
+  }
